da0d89ad07a00b7679a68ee2ba24515a11e484b0
Nur Ahmed Omar <nuuraxmed.8804@gmail.com>
2024-12-02 20:32:57 +0300

    Initial commit with Somali Book Store full project

---
+++ README.md
@@ -0,0 +1,3 @@
+This project is full stack mearn somali book store and have crud functionality completly create , edit  and delete books and details .
+
+this helps to store and orginize our book store with less time and more productive .
---
+++ backend/config.js
@@ -0,0 +1,10 @@
+export const PORT = 5555;
+
+// export const mongoDBURL =
+//   'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+export const mongoDBURL =
+  'mongodb+srv://nuuraxmed8804:WIUhRhqdveH7l7tg@cluster0.ikiaq.mongodb.net/Diiwaan?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
